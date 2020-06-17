0db5cdcf2719e4a4afe6145a70b590eef5bddf3e
unknown <patricktrindade9@gmail.com>
2020-06-17 12:19:09 -0300

    arquivo index atualizado, agora utilizando ARRAYS

--- index.js
+++ index.js
@@ -2,28 +2,27 @@
 //das notas entre os alunos e envia
 // msg do cálculo da média.
 
+//Trabalhando com ARRAYS (vetores (coleção de objetos)), para acesso utilizar nome da array[posição(lembrando que inicia em 0)]
+//para exemplo, verificar const media abaixo: alunos[0].nota, o programa vai dentro da array alunos, procura pela posição 0,
+//que é o primeiro objeto, e procura o atributo nota nesse objeto, assim fazendo o return do valor.
+const alunos = [
+    {
+        nome: "Patrick",
+        nota: 9.8
+    },
+    {
+        nome: "Juliana",
+        nota: 10
+    },
+    {
+        nome: "Joao",
+        nota: 2
+    }
+]
 
-//trabalhando com objetos (conjunto de propriedades, podendo receber valores ou funcionalidades, exemplo
-//const banda01 = {nome: "megadeth", genero: "Trash Metal"}) Para acessar, utilizar o nome da constante seguido
-//de ponto, exemplo: banda01.genero;
-const aluno01 = {
-    nome: "Patrick",
-    nota: 9.8
-}
 
-const aluno02 = {
-    nome: "Juliana",
-    nota: 10
-}
-
-const aluno03 = {
-    nome: "Joao",
-    nota: 2
-}
-
-console.log(aluno01)
-
-const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3
+console.log(alunos)
+const media = (alunos[0].nota + alunos[1].nota + alunos[2].nota) / 3
 
 
 //Se a média for maior que 5 parabenizar a turma
