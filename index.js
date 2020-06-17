9ed2459165e6bc36dfbe325c3cb7ddf03abfc5fc
unknown <patricktrindade9@gmail.com>
2020-06-17 11:52:38 -0300

    arquivo index, agora utilizando objetos

--- index.js
+++ index.js
@@ -2,14 +2,28 @@
 //das notas entre os alunos e envia
 // msg do cálculo da média.
 
-const aluno01 = "Patrick"
-const notaAluno01 = 9.8 
 
-const aluno02 = 'Juliana'
-const notaAluno02 = 10
+//trabalhando com objetos (conjunto de propriedades, podendo receber valores ou funcionalidades, exemplo
+//const banda01 = {nome: "megadeth", genero: "Trash Metal"}) Para acessar, utilizar o nome da constante seguido
+//de ponto, exemplo: banda01.genero;
+const aluno01 = {
+    nome: "Patrick",
+    nota: 9.8
+}
 
-const aluno03 = 'Joao'
-const notaAluno03 = 2
+const aluno02 = {
+    nome: "Juliana",
+    nota: 10
+}
+
+const aluno03 = {
+    nome: "Joao",
+    nota: 2
+}
+
+console.log(aluno01)
+
+const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3
 
 const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
 //Se a média for maior que 5 parabenizar a turma
@@ -19,6 +33,7 @@ if (media > 5){
 } else {
     console.log('A média foi menor que 5')
 }
-//add comentário para teste
+
+
 
 
